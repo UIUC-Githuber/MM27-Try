b069dcff1c72bf2f95fcbb9f97674b4769e65a87
ETwilight <hellobrian18@163.com>
2022-09-29 21:21:28 -0500

    Copy of MechMania-27 repo to this repo. Deleted the original .git folder so that new changes can be merged.

---
+++ app/src/main/java/mech/mania/competitor/model/decisions/ActionDecision.java
@@ -0,0 +1,8 @@
+package mech.mania.competitor.model.decisions;
+
+public abstract class ActionDecision {
+
+    @Override
+    public abstract String toString();
+
+}
---
+++ app/src/main/java/mech/mania/competitor/model/decisions/DoNothingDecision.java
@@ -0,0 +1,9 @@
+package mech.mania.competitor.model.decisions;
+
+public class DoNothingDecision extends ActionDecision {
+    @Override
+    public String toString() {
+        return "do_nothing ";
+    }
+
+}
