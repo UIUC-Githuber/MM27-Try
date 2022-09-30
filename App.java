66f5b786306e54075c07d733aab6d494a662fdaa
ETwilight <hellobrian18@163.com>
2022-09-29 20:28:59 -0500

    Add a username file.

--- App.java
+++ App.java
@@ -1,14 +1,6 @@
 public class App {
-    
+
     public static void main(String[] args) throws Exception {
         System.out.println("Working?");
-        System.out.println("ETwilight");
-        
-        /*
-        for(int i=0; i<100; i++){
-        System.out.println(i);
-        }
-        */
-        
     }
 }
