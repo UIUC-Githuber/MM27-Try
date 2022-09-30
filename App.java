5b0c996917390bb59294e66902f79f31e05c02aa
ETwilight <hellobrian18@163.com>
2022-09-29 20:27:34 -0500

    ETwilight's

--- App.java
+++ App.java
@@ -1,7 +1,8 @@
 public class App {
+    
     public static void main(String[] args) throws Exception {
         System.out.println("Working?");
-        
+        System.out.println("ETwilight");
         
         /*
         for(int i=0; i<100; i++){
