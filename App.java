4ded2d97bc14f956565598e9040e3ca232db6caa
Jingxil2 <elanna_liang@outlook.com>
2022-09-29 20:18:58 -0500

    Try1

---
+++ App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Working?");
+        
+        
+        /*
+        for(int i=0; i<100; i++){
+        System.out.println(i);
+        }
+        */
+        
+    }
+}
